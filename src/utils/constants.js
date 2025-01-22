962bc797fe48e1b4392e943d3d6352526c2df96a
patrion <patrick.s.davis@gmail.com>
2025-01-22 16:05:02 +0900

    Raffle logic Fixes #12

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const PHASES = {
+  ACTIVE: "Active", // Raffle is open for participants to submit entries
+  SETTLED: "Settled", // Entry period has ended, and winners are being processed
+  FINALIZED: "Finalized", // Raffle is confirmed complete; prizes are distributed
+};
