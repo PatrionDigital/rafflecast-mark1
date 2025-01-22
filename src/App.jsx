69568412dfc081a159889a6d3b97b79e63f701e7
patrion <patrick.s.davis@gmail.com>
2025-01-22 23:02:34 +0900

    Fix issues with Routing not displaying NavLinks

--- src/App.jsx
+++ src/App.jsx
@@ -13,51 +13,58 @@ import BrowseRafflesPage from "./pages/BrowseRafflesPage";
 
 import "./App.css";
 import BrowseEntriesPage from "./pages/BrowseEntriesPage";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <ParentLayout />,
-    handle: {
-      navLinks: [
-        { path: "/creator", label: "Creator" },
-        { path: "/entrant", label: "Entrant" },
+
+//const basename = import.meta.env.MODE === "production" ? "/rafflecast-mark1" : "";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <ParentLayout
+          navLinks={[
+            { path: "/creator", label: "Creator" },
+            { path: "/entrant", label: "Entrant" },
+          ]}
+        />
+      ),
+      children: [
+        { index: true, element: <LandingPage /> },
+        {
+          path: "entrant/*",
+          element: <EntrantDashboardLayout />,
+          handle: {
+            navLinks: [
+              { path: "/creator", label: "Creator" },
+              { path: "/entrant/raffles/browse", label: "Browse Raffles" },
+              { path: "/entrant/raffles/entered", label: "My Raffles" },
+            ],
+          },
+          children: [
+            { path: "raffles/browse", element: <BrowseRafflesPage /> },
+            { path: "raffles/entered", element: <BrowseEntriesPage /> },
+          ],
+        },
+        {
+          path: "creator/*",
+          element: <CreatorDashboardLayout />,
+          handle: {
+            navLinks: [
+              { path: "/entrant", label: "Entrant" },
+              { path: "/creator/raffles/new", label: "Create Raffle" },
+              { path: "/creator/raffles/manage", label: "Manage Raffles" },
+            ],
+          },
+          children: [
+            { path: "raffles/new", element: <CreateRafflePage /> },
+            { path: "raffles/manage", element: <ManageRafflesPage /> },
+          ],
+        },
       ],
     },
-    children: [
-      { index: true, element: <LandingPage /> },
-      {
-        path: "entrant/*",
-        element: <EntrantDashboardLayout />,
-        handle: {
-          navLinks: [
-            { path: "/creator", label: "Creator" },
-            { path: "/entrant/raffles/browse", label: "Browse Raffles" },
-            { path: "/entrant/raffles/entered", label: "My Raffles" },
-          ],
-        },
-        children: [
-          { path: "raffles/browse", element: <BrowseRafflesPage /> },
-          { path: "raffles/entered", element: <BrowseEntriesPage /> },
-        ],
-      },
-      {
-        path: "creator/*",
-        element: <CreatorDashboardLayout />,
-        handle: {
-          navLinks: [
-            { path: "/entrant", label: "Entrant" },
-            { path: "/creator/raffles/new", label: "Create Raffle" },
-            { path: "/creator/raffles/manage", label: "Manage Raffles" },
-          ],
-        },
-        children: [
-          { path: "raffles/new", element: <CreateRafflePage /> },
-          { path: "raffles/manage", element: <ManageRafflesPage /> },
-        ],
-      },
-    ],
-  },
-]);
+  ],
+  { basename: "/rafflecast-mark1" }
+);
 
 function App() {
   return <RouterProvider router={router} />;
