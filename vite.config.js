3f3c17ae881ad2cc8590b513e0364c34f127a9d8
patrion <patrick.s.davis@gmail.com>
2025-04-17 20:37:23 +0900

    Landing page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
+import tailwindcss from "@tailwindcss/vite";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -13,7 +15,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react()],
+    plugins: [tailwindcss(), react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
