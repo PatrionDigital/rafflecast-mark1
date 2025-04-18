52151ac1854a5a839e182e6062c1991a20d400e2
patrion <patrick.s.davis@gmail.com>
2025-04-18 10:24:09 +0900

    Install Windmill for dashboard component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@windmill/react-ui/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cochineal-red": "#c82a54",
+        "fabric-red": "#e25167",
+        "enamel-red": "#b2004b",
+        asphalt: "#353e34",
+        cement: "#a89e99",
+        black: "#130012",
+        "dark-rose": "#af3a6b",
+        "pastel-rose": "#f9d6d3",
+      },
+    },
   },
   plugins: [],
 };
